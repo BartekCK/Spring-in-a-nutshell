0376cf1e525be29fc62d2431319c2bbdc2c7a9b4
Bartek <bkotarski1@gmail.com>
2019-09-14 20:02:17 +0200

    PPrefis and suffix better

--- src/main/java/com/example/demospring/HomeController.java
+++ src/main/java/com/example/demospring/HomeController.java
@@ -5,20 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home(){
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
     public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2")int j){
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("result.jsp");
+        mv.setViewName("result");
         mv.addObject("num3",i+j);
         return mv;
     }
