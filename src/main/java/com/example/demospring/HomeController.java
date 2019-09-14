add1a809cdfb62025d0607595145824b47e7212c
Bartek <bkotarski1@gmail.com>
2019-09-14 17:18:44 +0200

    Model and View

--- src/main/java/com/example/demospring/HomeController.java
+++ src/main/java/com/example/demospring/HomeController.java
@@ -3,9 +3,8 @@ package com.example.demospring;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -17,9 +16,10 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int i, @RequestParam("num2")int j, HttpSession session){
-        int num3 = i+j;
-        session.setAttribute("num3",num3);
-        return "result.jsp";
+    public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2")int j){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("result.jsp");
+        mv.addObject("num3",i+j);
+        return mv;
     }
 }
