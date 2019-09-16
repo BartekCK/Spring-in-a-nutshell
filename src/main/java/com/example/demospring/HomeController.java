e6371a1de38191d3fdb52ac05d06bf138766daff
Bartek <bkotarski1@gmail.com>
2019-09-16 15:57:21 +0200

    Get Mapping

--- src/main/java/com/example/demospring/HomeController.java
+++ src/main/java/com/example/demospring/HomeController.java
@@ -4,10 +4,10 @@ import com.example.demospring.model.Alien;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
@@ -23,11 +23,11 @@ public class HomeController {
         return "index";
     }
 
-    @RequestMapping("add")
-    public String add(@RequestParam("num1") int i, @RequestParam("num2")int j, ModelMap m){
-
-        m.addAttribute("num3",i+j);
-        return "result";
+    @GetMapping("getAliens")
+    public String getAliens(Model m){
+        List <Alien> aliens = Arrays.asList(new Alien(101,"a"), new Alien(102,"b"));
+        m.addAttribute("result",aliens);
+        return "showAliens";
     }
 
     @PostMapping("addAlien")
