019b31108aa16deaa14064e4e307d2290e4dd2f9
Bartek <bkotarski1@gmail.com>
2019-10-30 16:19:59 +0100

    Add AfterReturning and AfterThrowing

--- src/main/java/com/example/demospring/LoggingAspect.java
+++ src/main/java/com/example/demospring/LoggingAspect.java
@@ -2,8 +2,7 @@ package com.example.demospring;
 
 import com.example.demospring.controller.AlienController;
 import com.example.demospring.model.Alien;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,8 +12,25 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
     @Before("execution(public * com.example.demospring.controller.AlienController.getAliens())")
-    public void getLog(){
-        LOGGER.info("getAlines() method call");
+    public void logBefore(){
+        LOGGER.info("getAlines() method will call");
+    }
+
+
+    @After("execution(public * com.example.demospring.controller.AlienController.getAliens())")
+    public void logAfterWithException(){
+        LOGGER.info("getAlines() method was called but maybe an exception occurred ");
+    }
+
+    @AfterReturning("execution(public * com.example.demospring.controller.AlienController.getAliens())")
+    public void logAfter(){
+        LOGGER.info("getAlines() method was called without exception");
+    }
+
+    @AfterThrowing("execution(public * com.example.demospring.controller.AlienController.getAliens())")
+    public void logException(){
+        LOGGER.warn("EXCEPTION");
     }
 }
